417ffcf31d3747eb253a69e69bb5a9d257b4b96c
Jerry-Yu-TeslaCoil <jerry.050222@outlook.com>
2025-10-13 10:58:11 +0800

    feat: Finish public VO Builder, VO filter functional
    # Modification
    - Add parameter prize to CardPlayer to maintain
    - Finish ClassicPlayerPublicVOBuilder, now VO state-based info filter is available
    - Add playerInvestment maintaining code to PlayerUtil

--- src/main/java/table/vo/publicinfo/builder/impl/ClassicPlayerPublicVOBuilder.java
+++ src/main/java/table/vo/publicinfo/builder/impl/ClassicPlayerPublicVOBuilder.java
@@ -1,5 +1,6 @@
 package table.vo.publicinfo.builder.impl;
 
+import table.card.PokerCard;
 import table.player.CardPlayer;
 import table.state.gamestate.GameState;
 import table.vo.publicinfo.PlayerPublicVO;
@@ -37,19 +38,23 @@ public class ClassicPlayerPublicVOBuilder implements PlayerPublicVOBuilder {
 
     @Override
     public PlayerPublicVO build(CardPlayer cardPlayer) {
-        PlayerPublicVO.Builder builder = PlayerPublicVO.builder();
+        PlayerPublicVO.Builder builder = PlayerPublicVO.builder()
+                .setPlayerPersonalVO(cardPlayer.getPlayerController().getPlayerPersonalVO())
+                .setChipsInHand(cardPlayer.getStack()).setContinuingGame(cardPlayer.getIsContinuingGame());
         return switch (gameState) {
-            case INIT -> builder.setPlayerPersonalVO(cardPlayer.getPlayerController().getPlayerPersonalVO())
-                    .setChipsInHand(cardPlayer.getStack())
-                    .setContinuingGame(cardPlayer.getIsContinuingGame())
+            case INIT -> builder.build();
+            case PRE_FLOP, FLOP, TURN, RIVER -> builder.setChipsInvested(cardPlayer.getPlayerInvestment())
                     .build();
-            case PRE_FLOP -> null;
-            case FLOP -> null;
-            case TURN -> null;
-            case RIVER -> null;
-            case SHOWDOWN -> null;
-            case END -> null;
+            case SHOWDOWN -> {
+                builder = builder.setChipsInvested(cardPlayer.getPlayerInvestment());
+                if (cardPlayer.getIsContinuingGame()) {
+                    builder.setPokerCard(cardPlayer.getHoleCards().toArray(new PokerCard[0]));
+                }
+                yield builder.build();
+            }
+            case END -> builder.setChipsInvested(cardPlayer.getPlayerInvestment())
+                    .setPrize(cardPlayer.getPlayerPrize())
+                    .build();
         };
-        //TODO: Finish build
     }
 }
